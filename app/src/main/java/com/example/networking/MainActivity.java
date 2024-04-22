711a17928af3a8b8239b3549d4af2e32cc901fde
a23alera <a23alera@student.his.se>
2024-04-22 14:40:00 +0200

    La till en private RecyclerView adapter i main acitivty.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -5,12 +5,14 @@ import android.util.Log;
 import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
 
+    private RecyclerView.Adapter<?> adapter;
     private ArrayList<Mountain> mountainsList;
 
     @Override
