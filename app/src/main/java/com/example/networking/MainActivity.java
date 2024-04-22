13018846c7460eacc944994caa7f6e499ed8c2ed
a23alera <a23alera@student.his.se>
2024-04-22 14:28:11 +0200

    skapa en klass för Mountain och sedan dekalerade array listen i Main klasse.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,19 +2,23 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
-
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+
+    private ArrayList<Mountain> mountainsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mountainsList = new ArrayList<>();
 
         new JsonFile(this, this).execute(JSON_FILE);
     }
