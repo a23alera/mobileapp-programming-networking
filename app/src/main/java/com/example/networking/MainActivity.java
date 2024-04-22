3bbed3a1cf0643ce6fdfb839f962f80d68f949cb
a23alera <a23alera@student.his.se>
2024-04-22 16:34:59 +0200

    Uppdaterade MainActivity så att den inkluderar och hanterar RecyclerView med en array list och även en anpassad adapter

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.networking;
 
 import android.os.Bundle;
-import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
+public class MainActivity extends AppCompatActivity {
 
-@SuppressWarnings("FieldCanBeLocal")
-public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
-    private final String JSON_FILE = "mountains.json";
-
-    private RecyclerView.Adapter<?> adapter;
+    private RecyclerView recyclerView;
+    private MountainAdapter adapter;
     private ArrayList<Mountain> mountainsList;
 
     @Override
@@ -20,14 +17,11 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         mountainsList = new ArrayList<>();
-
-        new JsonFile(this, this).execute(JSON_FILE);
+        adapter = new MountainAdapter(mountainsList);
+        recyclerView.setAdapter(adapter);
     }
-
-    @Override
-    public void onPostExecute(String json) {
-        Log.d("MainActivity", json);
-    }
-
 }
