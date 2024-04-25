fa0d28f33e939b3b5cdb60f7a80b24c5b4938d6a
a23alera <a23alera@student.his.se>
2024-04-25 18:20:49 +0200

    la till så RecyclerView Initiera  och adapter, hämta JSON-data, lägg till bergsnamn i lista, uppdatera RecyclerView,Konfigurera RecyclerView, hämta JSON-data och uppdatera med bergsnamn.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
+    private final String JSON_URL = "https://raw.githubusercontent.com/algolia/datasets/master/airports/airports.json";
     private RecyclerView recyclerView;
     private MountainAdapter adapter;
     private ArrayList<Mountain> mountainsList;
@@ -17,11 +23,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         mountainsList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
         adapter = new MountainAdapter(mountainsList);
         recyclerView.setAdapter(adapter);
+
+        new JsonTask(this).execute(JSON_URL);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String name = jsonObject.getString("name");
+                mountainsList.add(new Mountain(name));
+            }
+            adapter.notifyDataSetChanged();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
